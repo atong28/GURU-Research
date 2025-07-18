6761e02644dd388d06b249180ba1a6913462694c
Anthony Tong <atong28.usa@gmail.com>
2025-07-17 23:05:51 -0700

    update

---
+++ run_cross_att.sh
@@ -0,0 +1,30 @@
+cd /Users/atong/Documents/UCSD/Projects/GURU/GURU-Research
+experiment=e-spectre-crossatt
+input_types="{hsqc,c_nmr,h_nmr,mw}"
+requires="hsqc"
+for seed in 1; do
+    experiment_name="${experiment}-seed-${seed}"
+    sed -i '' "4s/.*/  name: atong-${experiment_name} /" jobs/atong_split_exp.yml
+    sed -i '' "45s/.*/              torchrun --nproc_per_node=6 cross_attention_main.py --input_types ${input_types} --requires ${requires} --seed ${seed} --experiment_name ${experiment_name} /" jobs/atong_split_exp.yml
+    kubectl create -f jobs/atong_split_exp.yml
+done
+
+experiment=e-spectre-ms-crossatt
+input_types="{hsqc,c_nmr,h_nmr,mw,mass_spec}"
+requires="hsqc"
+for seed in 1; do
+    experiment_name="${experiment}-seed-${seed}"
+    sed -i '' "4s/.*/  name: atong-${experiment_name} /" jobs/atong_split_exp.yml
+    sed -i '' "45s/.*/              torchrun --nproc_per_node=6 cross_attention_main.py --input_types ${input_types} --requires ${requires} --seed ${seed} --experiment_name ${experiment_name} /" jobs/atong_split_exp.yml
+    kubectl create -f jobs/atong_split_exp.yml
+done
+
+experiment=e-all-inputs-crossatt
+input_types="{hsqc,c_nmr,h_nmr,mw,mass_spec,iso_dist}"
+requires="hsqc"
+for seed in 1; do
+    experiment_name="${experiment}-seed-${seed}"
+    sed -i '' "4s/.*/  name: atong-${experiment_name} /" jobs/atong_split_exp.yml
+    sed -i '' "45s/.*/              torchrun --nproc_per_node=6 cross_attention_main.py --input_types ${input_types} --requires ${requires} --seed ${seed} --experiment_name ${experiment_name} /" jobs/atong_split_exp.yml
+    kubectl create -f jobs/atong_split_exp.yml
+done
