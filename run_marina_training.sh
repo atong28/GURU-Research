8cd4629924edba5794b9fb610058d540e9663451
Anthony Tong <atong28.usa@gmail.com>
2025-11-03 15:55:36 -0800

    updates

--- run_marina_training.sh
+++ run_marina_training.sh
@@ -1,9 +1,9 @@
 cd /Users/atong/Documents/UCSD/Projects/GURU/GURU-Research
-experiment="marina-run"
+experiment="marina-run-no-warmup-fixed-logs"
 input_types="{hsqc,c_nmr,h_nmr,mw,mass_spec}"
-for seed in 0 1 2 3 4 5; do
-    experiment_name="${experiment}-seed-${seed}"
+for seed in 0 1 2; do
+    experiment_name="${experiment}-num-${seed}"
     sed -i '' "4s/.*/  name: atong-${experiment_name} /" jobs/atong_marina_job.yml
-    sed -i '' "39s/.*/              pixi run --manifest-path pixi.toml -- torchrun --nproc_per_node=4 stage1.py --input_types ${input_types} --seed ${seed} --experiment_name ${experiment_name} --num_workers 4 /" jobs/atong_marina_job.yml
+    sed -i '' "41s/.*/              pixi run --manifest-path pixi.toml -- torchrun --nproc_per_node=4 stage1.py --input_types ${input_types} --seed 0 --experiment_name ${experiment_name} --no_warmup /" jobs/atong_marina_job.yml
     kubectl create -f jobs/atong_marina_job.yml
 done
